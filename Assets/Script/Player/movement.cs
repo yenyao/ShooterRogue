849fb7958e52a6d7db2cedae78e60f612c4b2db7
yenyao <yenyao@hotmail.co.uk>
2022-12-24 02:18:57 +0000

    player Aim

--- Assets/Script/Player/movement.cs
+++ Assets/Script/Player/movement.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float moveSpeed;
+    public Rigidbody2D rb;
     public Camera cam;
     private Vector2 playerMovement;
     private Vector2 mousePos;
-    private CharacterController controller;
-    // Start is called before the first frame update
-    void Start()
-    {
-        controller = gameObject.AddComponent<CharacterController>();
-    }
 
-    // Update is called once per frame
     void Update()
     {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         playerMovement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
     }
     void FixedUpdate() {
-        controller.Move(playerMovement * Time.fixedDeltaTime * moveSpeed);
+        handleAim();
+        handleMovement();
+    }
+
+    private void handleMovement() {
+        rb.MovePosition(rb.position + playerMovement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void handleAim() {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 }
